12b7c475e50e4f6bc70a9edf9e856745cc6bce75
desaijay315 <desai26jay@gmail.com>
2024-03-17 20:59:56 +0530

    add attendance tracking microservice and api-gateway

---
+++ AttendanceTrackingService/src/main/java/com/hrms/AttendanceTrackingService/AttendanceTrackingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hrms.AttendanceTrackingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceTrackingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceTrackingServiceApplication.class, args);
+	}
+
+}
---
+++ AttendanceTrackingService/src/test/java/com/hrms/AttendanceTrackingService/AttendanceTrackingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrms.AttendanceTrackingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceTrackingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
